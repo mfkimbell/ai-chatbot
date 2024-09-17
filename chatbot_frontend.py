9150ddbc5ca374f60e92f8c8a135bdeae3bb9d21
mitchell_kimbell <mfkimbell@gmail.com>
2024-09-17 09:20:05 -0500

    removed unnecessary logs

--- chatbot_frontend.py
+++ chatbot_frontend.py
@@ -5,25 +5,21 @@ import streamlit as st
 import  chatbot_backend as demo  #**Import your Chatbot file as demo
 
 
-#2 Set Title for Chatbot - https://docs.streamlit.io/library/api-reference/text/st.title
 st.title("Hi, This is Chatbot Mitch :sunglasses:") # **Modify this based on the title you want in want
 
-#3 LangChain memory to the session cache - Session State - https://docs.streamlit.io/library/api-reference/session-state
+#https://docs.streamlit.io/library/api-reference/session-state
 if 'memory' not in st.session_state: 
     st.session_state.memory = demo.demo_memory() #** Modify the import and memory function() attributes initialize the memory
 
-#4 Add the UI chat history to the session cache - Session State - https://docs.streamlit.io/library/api-reference/session-state
+#https://docs.streamlit.io/library/api-reference/session-state
 if 'chat_history' not in st.session_state: #see if the chat history hasn't been created yet
     st.session_state.chat_history = [] #initialize the chat history
 
-#5 Re-render the chat history (Streamlit re-runs this script, so need this to preserve previous chat messages)
 # basically for streamlit, the history only keeps track of the data, not the ui elements
 # so we have to regenerate ui elements every render
 for message in st.session_state.chat_history: 
     with st.chat_message(message["role"]): 
         st.markdown(message["text"]) 
-
-#6 Enter the details for chatbot input box 
      
 input_text = st.chat_input("Powered by Bedrock and Claude") # **display a chat input box
 if input_text: 
